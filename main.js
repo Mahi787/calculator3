97f2e452507b9c6cd3d37cdf5dd9db2c8344e44d
Ishat Noor Mahi <71538842+Mahi787@users.noreply.github.com>
2020-10-11 19:08:29 +0600

    1st commit

---
+++ main.js
@@ -0,0 +1,25 @@
+        //define the result field
+var resultfield= $("#result");
+function insertNum(number){
+    var existingNumbers = resultfield.val();
+    resultfield.val( existingNumbers + number)
+}
+function clearResult(){
+    resultfield.val('')
+    document.getElementById("result").style.color = "black";
+    document.getElementById("result").style.border = "none";
+
+}
+function calculate(){
+  var result =  eval(resultfield.val())
+  resultfield.val(result);
+  document.getElementById("result").style.border = " 4px solid green";
+  document.getElementById("result").style.color = "red";
+  document.getElementById("result").style.fontFamily = "cursive";
+}
+function deleteNum(){
+    var value = resultfield.val();
+    if(value!= ''){
+        resultfield.val(value.slice(0,-1));
+    }
+}
